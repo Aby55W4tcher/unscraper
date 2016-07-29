897b47ed2bbca8c1d58625b8f4233745311f567a
Aly <alyzeinmohamed@gmail.com>
2016-07-29 18:20:44 +0000

    pagination need fixing

--- Test.py
+++ Test.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-url = 'http://www.reliefweb.int/updates?sl=environment-report_listing%252Ctaxonomy_index_tid_source-1503%252Ctaxonomy_index_tid_content_format-8'
-base_url = 'http://www.reliefweb.int'
+url = 'https://www.reliefweb.int/updates?sl=environment-report_listing%252Ctaxonomy_index_tid_source-1503%252Ctaxonomy_index_tid_content_format-8'
+base_url = 'https://www.reliefweb.int'
 final_list = []
 
 def get_next_link(link):
     r = requests.get(link)
     soup = bs(r.text, "html5lib")
-    elm = soup.find('div',{'class':'pager-current'})
+    elm = soup.find('div',{'class':'pager'})
     next_ = elm.findNext('a')
     next_page_link = base_url + next_['href']
     if next_page_link:
