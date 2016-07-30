23af4e7a0b807669c3ebf9442a1ec9cddaf83412
Aly <alyzeinmohamad@gmail.com>
2016-07-30 06:29:11 +0000

    pagination update

--- paginationFunction.py
+++ paginationFunction.py
@@ -10,8 +10,9 @@ def get_next_link(link):
     r = requests.get(link)
     soup = bs(r.text, "html5lib")
     elm = soup.find('div',{'class':'pager'})
-    finding = elm.findNext ('li',{'class':'pager-current first'})
-    next_ = finding.findNext('a')
+    finding = elm.findNext ('ul',{'class':'links pager pager-list'})
+    finding_ = finding.findNext ('li',{'class':'pager-current'})
+    next_ = finding_.findNext('a')
     next_page_link = base_url + next_['href']
     if next_page_link:
         print next_page_link
